32613914b2b9b147f0cf7c744960dab479413e0f
Vladimir Vrzić <vvrzic@gmail.com>
2025-08-14 18:52:15 +0200

    Added missing dictionary defaults guards

--- meritmonitor/meritstore.py
+++ meritmonitor/meritstore.py
@@ -12,18 +12,29 @@ class MeritStore:
     def add_personal(self, system: str, amount: int):
         this_week = weekly_key()
         self.live_personal_by_system.setdefault(this_week, {}).setdefault(system, 0)
+
         self.live_personal_by_system[this_week][system] += amount
 
     def add_control_points(self, system:str, amount: int):
         this_week = weekly_key()
         self.live_control_points_by_system.setdefault(this_week, {}).setdefault(system, 0)
+
         self.live_control_points_by_system[this_week][system] += amount
 
     def sum_personal(self):
-        return sum(self.live_personal_by_system[weekly_key()].values())
+        this_week = weekly_key()
+        self.live_personal_by_system.setdefault(this_week, {})
+
+        return sum(self.live_personal_by_system[this_week].values())
 
     def sum_system(self):
+        this_week = weekly_key()
+        self.live_control_points_by_system.setdefault(this_week, {})
+
         return sum(self.live_control_points_by_system[weekly_key()].values())
 
     def get_current_merits_by_system(self):
-        return self.live_personal_by_system[weekly_key()]
+        this_week = weekly_key()
+        self.live_personal_by_system.setdefault(this_week, {})
+
+        return self.live_personal_by_system[this_week]
